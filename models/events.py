3181f30ae56c4074a3654963cc4150b858413a33
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-10 14:41:36 +0300

    Added a schema_extra to the Config subclass

--- models/events.py
+++ models/events.py
@@ -15,3 +15,15 @@ class Event(BaseModel):
     description: str
     tags: List[str]
     location: str
+
+    class Config:
+        """The config for the Event class"""
+        schema_extra = {
+            "example": {
+                "title": "FastAPI book launch",
+                "image": "https://linktomyimage.com/image.png",
+                "description": "We will be discussing the contents of the FastAPI book in this event. Ensure to come with your own copy to win gifts!",
+                "tags": ["python", "fastapi", "book", "launch"],
+                "location": "Google Meet"
+            }
+        }
