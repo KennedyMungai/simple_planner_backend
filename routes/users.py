9eca3161c20723fd1d20b5200463c416916e0368
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-11 10:12:30 +0300

    Wrote the signup route

--- routes/users.py
+++ routes/users.py
@@ -1,9 +1,32 @@
 """The users route file"""
 from fastapi import APIRouter, HTTPException, status
 
-from models.user import User, UserSignIn
+from models.users import User, UserSignIn
 
 
 user_router = APIRouter(tags=["User"])
 
 users = {}
+
+
+@user_router.post('/signup')
+async def sign_new_user(data: UserSignIn) -> dict:
+    """The user sign in endpoint
+
+    Args:
+        data (Class): The data template for the new user
+
+    Returns:
+        dict: A response to calling the API endpoint
+    """
+    if data.email in users:
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail="The user with the supplied username exists"
+        )
+
+    users[data.email] = data
+
+    return {
+        "Message": "User successfully registered"
+    }
