232fadad9e1750bd1f832deaf26e66938861544d
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-11 09:59:08 +0300

    Defined the users dictionary and user_router route

--- routes/users.py
+++ routes/users.py
@@ -1,3 +1,9 @@
 """The users route file"""
 from fastapi import APIRouter, HTTPException, status
+
 from models.user import User, UserSignIn
+
+
+user_router = APIRouter(tags=["User"])
+
+users = {}
