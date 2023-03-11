e2c961fee419de97dcd18d4d1cfe4117c710e578
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-11 10:41:15 +0300

    Defined the Events root router

--- routes/events.py
+++ routes/events.py
@@ -7,3 +7,13 @@ from typing import List
 event_router = APIRouter(tags=["Events"])
 
 events = []
+
+
+@event_router.get("/")
+async def retrieve_all_events() -> List[Event]:
+    """The base route for the vent_router route
+
+    Returns:
+        List[Event]: Returns a list of events
+    """
+    return events
